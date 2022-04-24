578362b1f45e5f14a1a444c133aea667488aa74e
dgroch <daniel.j.groch@gmail.com>
2022-04-25 09:59:49 +1000

    Update header.php to verify domain for Facebook

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
 	<meta charset="<?php bloginfo('charset'); ?>" />
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<meta name="facebook-domain-verification" content="3ouecxfyorskezlqwllogzu6k0sqxu" />
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
